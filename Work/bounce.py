174d461210e13bdb8c080bae95cd6a9feca57c58
linghch <yangcq8@qq.com>
2026-09-21 17:59:30 +0800

    Add bounce exercise

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce = 0 # Bounce times
+ball_height = 100 # Initialize ball height (meters)
+
+while bounce < 10:
+    bounce = bounce + 1
+    ball_height = ball_height * 3 / 5
+    print(bounce, round(ball_height, ndigits=4))
