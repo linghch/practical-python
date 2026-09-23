4911ebc29b189b63ba3834b491518bf64921de6c
linghch <yangcq8@qq.com>
2026-09-23 17:38:17 +0800

    Add pcost exercise

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
-#
+# author linghch
 # Exercise 1.27
+
+total_cost = 0.0
+
+with open('Data/portfolio.csv') as f:
+    next(f) # skip the header
+    for line in f:
+        stock = line.strip().split(',') 
+        shares = int(stock[1])
+        price = float(stock[2])
+        total_cost += shares * price # Accumulate cost of each company's shares
+
+print(f'Total cost {round(total_cost, ndigits=2)}')
